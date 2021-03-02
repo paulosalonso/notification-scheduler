84d66a583513861dca7b65da1c640a9a64bfe6b2
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-03-01 23:23:51 -0300

    Added id in Notification domain entity

--- src/main/java/com/github/paulosalonso/notification/domain/Notification.java
+++ src/main/java/com/github/paulosalonso/notification/domain/Notification.java
@@ -4,12 +4,14 @@ import lombok.*;
 
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
 @Builder
 public class Notification {
+    private final UUID id;
     private final Channel channel;
     private final OffsetDateTime scheduleAt;
     private final List<String> recipients;
