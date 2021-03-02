0b1999b33a30ebde0bde980b3bdefe61da4a47e7
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-03-01 22:29:24 -0300

    Initial commit. This sources was generated at start.spring.io

---
+++ src/main/java/com/github/paulosalonso/notification/NotificationSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/paulosalonso/notification/NotificationSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
