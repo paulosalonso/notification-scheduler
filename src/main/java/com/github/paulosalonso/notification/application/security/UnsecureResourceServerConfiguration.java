b3f09706f9628b093b3e90ad6d24fcf12b030499
Paulo Alonso <paulo_alonso_@hotmail.com>
2021-03-06 22:41:09 -0300

    Configuring actuator

--- src/main/java/com/github/paulosalonso/notification/application/security/UnsecureResourceServerConfiguration.java
+++ src/main/java/com/github/paulosalonso/notification/application/security/UnsecureResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.github.paulosalonso.notification.application.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 @Slf4j
 @Profile("default")
 @Configuration
-@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 public class UnsecureResourceServerConfiguration {
     public UnsecureResourceServerConfiguration() {
         log.info("Starting application without security");
